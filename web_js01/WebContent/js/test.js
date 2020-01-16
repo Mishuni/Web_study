2f5434fbd3267035e8aa15f7813174e99bbdccaf
Mishuni <miseon543@gmail.com>
2020-01-16 10:49:57 +0900

    Update web contents

---
+++ web_js01/WebContent/js/test.js
@@ -0,0 +1,9 @@
+document.write("<br>");
+document.write("javascript 외부파일로 저장하고 링크처리함<br>");
+document.write("javascript 대소문자 구분한다1111<br>");
+document.write("<font color='red' size='5'>javascript를 이용한 출력3</font>");
+
+function hello() {
+	alert("hi hi");
+	console.log("lunch!")
+}
